2b5e1940415e3c3bf61eab6e52b7107293f50df7
mdnumanahmed <mnumansyl@gmail.com>
2024-10-26 20:14:31 +0600

    Latest Posts loaded from API and displayed in the UI with conditional rendering

--- js/app.js
+++ js/app.js
@@ -167,4 +167,76 @@ const showMarkAsRead = async (id) => {
     `;
 };
 
+const loadLatestPosts = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/retro-forum/latest-posts"
+  );
+  const data = await res.json();
+  const posts = data;
+  displayLatestPosts(posts);
+};
+
+const displayLatestPosts = (posts) => {
+  const postsContainer = document.getElementById("latest-post-container");
+  console.log(posts);
+  posts.forEach((post) => {
+    const { cover_image, title, profile_image, description, author } = post;
+    postsContainer.innerHTML += `
+      <div class="border-2 rounded-2xl p-6 space-y-4 shadow-md">
+        <img src="${cover_image}" alt="" class="rounded-2xl" />
+        <div class="space-y-3">
+            <p class="mulish text-base text-[#12132D99]">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-calendar2-date inline mr-3"
+                viewBox="0 0 16 16"
+            >
+                <path
+                d="M6.445 12.688V7.354h-.633A13 13 0 0 0 4.5 8.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23"
+                />
+                <path
+                d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1z"
+                />
+                <path
+                d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5z"
+                />
+            </svg>
+            ${author?.posted_date ? author.posted_date : "Date not available"}
+            </p>
+            <h3 class="text-lg font-extrabold">
+            ${title ? title : "Title not available"}
+            </h3>
+            <p class="text-base mulish text-[#12132D99]">
+            ${description ? description : "Description not available"}
+            </p>
+            <div class="divider"></div>
+            <div class="flex items-center gap-4">
+            <div class="avatar">
+                <div
+                class="ring-primary ring-offset-base-100 w-11 rounded-full ring ring-offset-2"
+                >
+                <img
+                    src="${profile_image ? profile_image : "Image not found"}"
+                />
+                </div>
+            </div>
+            <div>
+                <h4 class="mulish text-base font-bold">${
+                  author?.name ? author?.name : "Name not found"
+                }</h4>
+                <p class="text-sm mulish text-[#12132D99]">${
+                  author?.designation ? author.designation : "Unknown"
+                }</p>
+            </div>
+            </div>
+        </div>
+        </div>
+        `;
+  });
+};
+
 loadPosts();
+loadLatestPosts();
